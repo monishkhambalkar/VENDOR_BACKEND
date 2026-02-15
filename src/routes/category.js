c63771bfb7b89f9753e0cece3617112e1c88d25d
Monish Khambalkar <monish.khambalkar@plus91.in>
2026-02-15 10:27:45 +0530

    add swagger and pm2

--- src/routes/category.js
+++ src/routes/category.js
@@ -12,17 +12,100 @@ const categoryController = ({
 const router = express.Router();
 
 // Add Category
+
+/**
+ * @swagger
+ * /admin/api/category/add-category:
+ *   post:
+ *     summary: Add new category
+ *     tags:
+ *       - Category
+ *     parameters:
+ *       - in: query
+ *         name: categoryName
+ *         required: true
+ *         schema:
+ *           type: string
+ *         example: Electronics
+ *     responses:
+ *       200:
+ *         description: Category added successfully
+ */
+
 router.post("/add-category", categoryController.addCategory);
 
+/**
+ * @swagger
+ * /admin/api/category/update-category/{categoryID}:
+ *   patch:
+ *     summary: Update category
+ *     tags:
+ *       - Category
+ *     parameters:
+ *       - in: path
+ *         name: categoryID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Category updated
+ */
 router.patch("/update-category/:categoryID", categoryController.updateCategory);
 
+
+/**
+ * @swagger
+ * /admin/api/category/delete-category/{categoryID}:
+ *   delete:
+ *     summary: Delete category
+ *     tags:
+ *       - Category
+ *     parameters:
+ *       - in: path
+ *         name: categoryID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Category deleted
+ */
 router.delete(
   "/delete-category/:categoryID",
   categoryController.deleteCategory
 );
 
+/**
+ * @swagger
+ * /admin/api/category/select-category:
+ *   post:
+ *     summary: Get all categories
+ *     tags:
+ *       - Category
+ *     responses:
+ *       200:
+ *         description: Category list fetched
+ */
 router.post("/select-category", categoryController.selectCategory);
 
+/**
+ * @swagger
+ * /admin/api/category/select-category/{categoryID}:
+ *   post:
+ *     summary: Get category by ID
+ *     tags:
+ *       - Category
+ *     parameters:
+ *       - in: path
+ *         name: categoryID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Category fetched successfully
+ */
 router.post(
   "/select-category/:categoryID",
   categoryController.selectOneCategory
--- src/routes/product.js
+++ src/routes/product.js
@@ -18,6 +18,17 @@ const upload = multer({
   limits: { fileSize: 3e7 },
 });
 
+/**
+ * @swagger
+ * /admin/api/product/add-product:
+ *   post:
+ *     summary: Add product
+ *     tags:
+ *       - Product
+ *     responses:
+ *       200:
+ *         description: Product added
+ */
 productRouter.post(
   "/add-product",
   authenticate,
@@ -25,19 +36,94 @@ productRouter.post(
   productController.addProduct
 );
 
+/**
+ * @swagger
+ * /admin/api/product/update-product/{iProductID}:
+ *   patch:
+ *     summary: Update product
+ *     tags:
+ *       - Product
+ *     parameters:
+ *       - in: path
+ *         name: iProductID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Product updated
+ */
 productRouter.patch(
   "/update-product/:iProductID",
   upload.fields([{ name: "file", maxCount: 3 }]),
   productController.updateProduct
 );
 
+/**
+ * @swagger
+ * /admin/api/product/delete-product/{iProductID}:
+ *   delete:
+ *     summary: Delete product
+ *     tags:
+ *       - Product
+ *     parameters:
+ *       - in: path
+ *         name: iProductID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Product deleted
+ */
 productRouter.delete(
   "/delete-product/:iProductID",
   productController.deleteProduct
 );
 
+/**
+ * @swagger
+ * /admin/api/product/select-product:
+ *   post:
+ *     summary: Get all products
+ *     tags:
+ *       - Product
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: number
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: number
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Products fetched
+ */
 productRouter.post("/select-product", productController.selectProduct);
 
+/**
+ * @swagger
+ * /admin/api/product/select-product/{iProductID}:
+ *   post:
+ *     summary: Get product by ID
+ *     tags:
+ *       - Product
+ *     parameters:
+ *       - in: path
+ *         name: iProductID
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Product fetched
+ */
 productRouter.post(
   "/select-product/:iProductID",
   productController.selectOneProduct
